c1f3657bef15e885e6d1250c08ee305b1d55e6a3
pikachungg <34949943+pikachungg@users.noreply.github.com>
2021-06-08 15:42:55 -0500

    finished

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+document.addEventListener('mousemove', (event) => {
+	let buttonY = document.querySelector("#no").getBoundingClientRect().top
+	let buttonX = document.querySelector("#no").getBoundingClientRect().left
+	let buttonZ = document.querySelector("#no").getBoundingClientRect().right
+	let buttonI = document.querySelector("#no").getBoundingClientRect().bottom
+
+	let buttonWidth = buttonX - buttonZ; 
+	let buttonHeight = buttonI - buttonY;
+
+	if (event.clientX >= buttonX && event.clientX <= buttonZ && event.clientY >= buttonY && event.clientY <= buttonI){
+		changePosition(buttonWidth, buttonHeight);
+	}
+});
+
+const iKnewIt = () => {
+	alert("I knew you were going to say yes 😍")
+}
+
+const changePosition = (elementWidth, elementHeight) => {
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+
+	let randomX = Math.floor(Math.random() * ((width + elementWidth)-50)) + elementWidth * -1;
+	let randomY = Math.floor(Math.random() * ((height - elementHeight) - 50)) + elementHeight;
+
+	randomX = randomX.toString() + "px";
+	randomY = randomY.toString() + "px";
+	document.getElementById("no").style.position = "absolute";
+	document.getElementById("no").style.left = randomX;
+	document.getElementById("no").style.top = randomY;
+}
